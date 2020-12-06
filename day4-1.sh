9e5a6ce33076d4ca3765e62a05e5701019ecbbaf
pspeegle <pspeegle@crimson.ua.edu>
2020-12-06 07:25:53 -0600

    Added data file

--- day4-1.sh
+++ day4-1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed 's/^$/*/' data4.txt | awk 1 ORS=' ' | awk 'BEGIN{RS="*"} !/byr/ {next} !/iyr/ {next} !/eyr/ {next} !/hgt/ {next} !/hcl/ {next} !/ecl/ {next} !/pid/ {next} {print}' | wc -l
+sed 's/^$/*/' data/data4.txt | awk 1 ORS=' ' | awk 'BEGIN{RS="*"} !/byr/ {next} !/iyr/ {next} !/eyr/ {next} !/hgt/ {next} !/hcl/ {next} !/ecl/ {next} !/pid/ {next} {print}' | wc -l
--- day5-1.sh
+++ day5-1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat data5.txt | tr "FBRL" "0110" | sort | tail -1 | sed 's/.*/2i&p/' | dc
+cat data/data5.txt | tr "FBRL" "0110" | sort | tail -1 | sed 's/.*/2i&p/' | dc
