3d1e37864efd2a8698c8fe3a578af7a98892e56b
pspeegle <pspeegle@crimson.ua.edu>
2020-12-11 10:47:51 -0600

    it's not a bad solution per se, but it's not good

--- day6-2.sh
+++ day6-2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed 's/^$/*/' data/data6.txt | awk 1 ORS=' ' | awk 'BEGIN{RS="*"} {for(i=1;i<=length($0);i++){a[substr($0,i,1)]=1} for(i in a){printf("%s",i)} print "";delete a}'
+sed 's/^$/*/' data/data6.txt | tr '\n' ' ' | awk 'BEGIN{RS="*"}{for(i=1;i<=length($1);i++){print("-"NF);for(j=2;j<=NF;j++){if ($j~substr($1,i,1)){print "x"}}}}' | awk 'BEGIN{RS="-"}NF==$1{print ""}' | wc -l
