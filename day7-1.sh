fd43e0a8d0e94fd6caf2ecd4051aab5276d37a44
pspeegle <pspeegle@crimson.ua.edu>
2020-12-13 10:32:39 -0600

    there IS a better way to subtract one!

--- day7-1.sh
+++ day7-1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-r() { eval "$(awk -v m="$1" '{if($0~".*c.*"m){print "r \""$1" "$2"\""}}' data/data7.txt)"; echo "$1";}; r "shiny gold" | sort | uniq | wc -l | echo $(cat)-1 | bc
+r() { eval "$(awk -v m="$1" '{if($0~".*c.*"m){print "r \""$1" "$2"\""}}' data/data7.txt)";echo "$1";}; r "shiny gold" | sort | uniq | sed 1d | wc -l
