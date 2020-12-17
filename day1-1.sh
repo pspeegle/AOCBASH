5102cbf978aa26efbb718a4d7045ab907b988c52
pspeegle <pspeegle@crimson.ua.edu>
2020-12-17 14:00:04 -0600

    YIKES

--- day1-1.sh
+++ day1-1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-awk 'BEGIN{RS="";FS="\n"}{for(i=1;i<=NF;i++){for(j=1;j<=i;j++){if($i+$j==2020){printf("%d\n",$i*$j)}}}}' data/data1.txt
+awk 'BEGIN{RS=""}{for(i=1;i<=NF;i++){for(j=1;j<=i;j++){if($i+$j==2020){printf("%d\n",$i*$j)}}}}' data/data1.txt
